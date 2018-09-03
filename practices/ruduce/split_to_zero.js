dd85d029cdbe46c5fa518d8b38b5725d1e5cb3c0
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 17:41:45 +0800

    Passed reduce

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let result = [number];
+  for(let num = number; num > 0; num -= interval) {
+    let tmp = parseFloat((num - interval).toFixed(1));
+    result.push(tmp);
+    if (tmp == 0) {
+      break;
+    }
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
