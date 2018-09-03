fa42c412bf74c2771680b6703a85c03aaf1ac583
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 15:59:13 +0800

    Passed flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let result = [];
+  collection.forEach(arr => {
+    if (typeof arr === 'number') {
+      result.push(arr);
+      return;
+    }
+    arr.forEach(num => {
+      result.push(num);
+    })
+  })
+  return result;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,9 @@
 'use strict';
+var to_one = require('./double_dimensional_array_to_one_1.js')
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+  let arr = to_one(collection);
+  return arr.filter((e, i) => arr.indexOf(e) == i)
 }
 
 module.exports = double_to_one;
