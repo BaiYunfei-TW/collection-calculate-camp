94294ac047e5c98ab3a964f5fae953c699cab6f6
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 23:11:28 +0800

    Finished collectionOperator

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,7 +1,27 @@
 'use strict';
 
+var get_integer_interval = require('./get_integer_interval.js')
+
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var numberArray = [];
+  if(number_a < number_b){
+    for(var v = number_a; v <= number_b; v++){
+      numberArray.push(v);
+    }
+  }else{
+    for(var v = number_a; v >= number_b; v--){
+      numberArray.push(v);
+    }
+  }
+  return numberArray.map(num => {
+    let result = '';
+    do {
+      num -= 1;
+      result = String.fromCharCode(97 + (num % 26)) + result;
+      num = Math.floor(num / 26);
+    } while(num > 0);
+    return result;
+  });
 }
 
 module.exports = get_letter_interval_2;
