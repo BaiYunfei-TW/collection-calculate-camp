8fabd9ecc5f6e8efd76492c7e7b32e9a994a8d24
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 22:06:28 +0800

    Finished section_3

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,13 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let result = [];
+  collection_a.forEach(data => {
+    if (object_b.value.includes(data.key)) {
+      result.push({key: data.key, count: data.count - Math.floor(data.count/3)});
+    } else {
+      result.push(data)
+    }
+  })
+  return result;
 }
 
 module.exports = create_updated_collection;
--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,5 +1,39 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  let countMap = {};
+  collection_a.forEach(e => {
+    let count = pareseKeyAndCount(e);
+    if (countMap[count.key] === undefined) {
+      countMap[count.key] = count.count;
+      return;
+    }
+    countMap[count.key] += count.count;
+  })
+  let result = [];
+  for(let key in countMap){
+    if (object_b.value.includes(key)) {
+      result.push({key:key, count:countMap[key] - Math.floor(countMap[key]/3)});
+      continue;
+    }
+    result.push({key:key, count:countMap[key]});
+  }
+  return result;
 }
+function pareseKeyAndCount(str) {
+  let count = {
+    key: str,
+    count: 1
+  }
+  if (str.includes('[')) {
+    count.key = str.substring(0, str.indexOf('['));
+    count.count = parseInt(str.substring(str.indexOf('[') + 1, str.indexOf(']')));
+  } else if (str.includes('-')) {
+    count.key = str.split('-')[0];
+    count.count = parseInt(str.split('-')[1]);
+  } else if (str.includes(':')) {
+    count.key = str.split(':')[0];
+    count.count = parseInt(str.split(':')[1]);
+  }
 
+  return count;
+}
 module.exports = create_updated_collection;
