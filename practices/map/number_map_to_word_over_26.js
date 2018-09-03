d39af198709999aab4fe8230ab03aea807dd7a85
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 23:41:30 +0800

    Implemented number_map_to_word_over_26

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,14 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map(num => {
+    let result = '';
+    do {
+      num -= 1;
+      result = String.fromCharCode(97 + (num % 26)) + result;
+      num = Math.floor(num / 26);
+    } while(num > 0);
+    return result;
+  })
 };
 
 module.exports = number_map_to_word_over_26;
