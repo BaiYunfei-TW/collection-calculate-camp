ccce3945a4ce1f60f9e3507bedd74a4550a7ee3f
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 22:56:44 +0800

    Finished subscript_is_even

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,5 @@
 'use strict';
 var is_exist_element = function(collection,element){
-
+  return collection.filter((num, index) => index % 2 === 0 && num === element).length != 0;
 };
 module.exports = is_exist_element;
