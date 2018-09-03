6837c7d0d19d7cc535df24ec3e7594f7647ed3cf
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 10:35:47 +0800

    Passed get_integer_interval_2

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  var result = [];
+  if(number_a < number_b){
+    for(var v = number_a; v <= number_b; v++){
+      if(v % 2 == 0){
+        result.push(v);
+      }
+    }
+  }else{
+    for(var v = number_a; v >= number_b; v--){
+      if (v % 2 == 0) {
+        result.push(v);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = get_integer_interval_2;
