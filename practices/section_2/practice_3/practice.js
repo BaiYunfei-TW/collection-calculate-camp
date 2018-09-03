a8d3c8816ba0dc2b807fd403a5a884a2fb9f3149
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-03 21:45:32 +0800

    Finished section_2

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,5 +1,37 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  let countMap = {};
+  collection.forEach(e => {
+    let count = pareseKeyAndCount(e);
+    if (countMap[count.key] === undefined) {
+      countMap[count.key] = count.count;
+      return;
+    }
+    countMap[count.key] += count.count;
+  })
+  let result = [];
+  for(let key in countMap){
+    result.push({name:key, summary:countMap[key]});
+  }
+  return result;
+}
+
+function pareseKeyAndCount(str) {
+  let count = {
+    key: str,
+    count: 1
+  }
+  if (str.includes('[')) {
+    count.key = str.substring(0, str.indexOf('['));
+    count.count = parseInt(str.substring(str.indexOf('[') + 1, str.indexOf(']')));
+  }else if (str.includes('-')) {
+    count.key = str.split('-')[0];
+    count.count = parseInt(str.split('-')[1]);
+  }else if (str.includes(':')) {
+    count.key = str.split(':')[0];
+    count.count = parseInt(str.split(':')[1]);
+  }
+
+  return count;
 }
 
 module.exports = count_same_elements;
